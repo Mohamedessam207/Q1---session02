a90989afa011d0df1ecfdb685e0a17784be79936
user <user@DESKTOP-9I94I4G>
2024-12-12 13:53:06 +0200

    Add project files.

---
+++ Q1 session02 $/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1_session02__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("you entered"+number);
+        }
+    }
+}
